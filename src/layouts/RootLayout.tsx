e5ce165ee65dc15fd8907284eeb3589643cd7a3c
SachinYadavAug20 <samtagon777@gmail.com>
2026-07-11 14:36:27 +0530

    Loading screen of nvim style splash screen

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,9 +1,18 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../sections/Footer";
 import { Toaster } from "../components/ui/sonner";
 
 const RootLayout = () => {
+  useEffect(() => {
+    const splash = document.getElementById("splash");
+    if (splash) {
+      splash.classList.add("fade-out");
+      setTimeout(() => splash.remove(), 550);
+    }
+  }, []);
+
   return (
     <>
       <Navbar />
