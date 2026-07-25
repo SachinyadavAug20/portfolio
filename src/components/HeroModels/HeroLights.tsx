982057d5d6ecf3e1a971f71206fd8e00c3e5821d
SachinYadavAug20 <samtagon777@gmail.com>
2026-07-25 13:59:20 +0530

    Added lighting based on tod

--- src/components/HeroModels/HeroLights.tsx
+++ src/components/HeroModels/HeroLights.tsx
@@ -1,5 +1,5 @@
 import { useMemo } from "react";
-import {getTimeOfDay} from "../../lib/utils";
+import { getTimeOfDay } from "../../lib/utils";
 
 const HeroLights = () => {
   const tod = useMemo(() => getTimeOfDay(), []);
@@ -9,42 +9,42 @@ const HeroLights = () => {
   return (
     <>
       <spotLight
-        position={[2, 5, 6]}
-        intensity={tod.intensity * 10}
-        angle={0.15}
-        penumbra={0.2}
+        position={[2, 3, 4]}
+        intensity={tod.intensity * 5}
+        angle={0.2}
+        penumbra={0.3}
         color={tod.color}
       />
       <spotLight
-        position={[4, 5, 6]}
-        angle={0.3}
-        intensity={accent(40)}
+        position={[3, 2, 4]}
+        angle={0.4}
+        intensity={accent(20)}
         penumbra={0.5}
         color="#4cc9f0"
       />
       <spotLight
-        position={[-3, 5, 5]}
-        angle={0.4}
-        intensity={accent(60)}
+        position={[-2, 3, 3]}
+        angle={0.5}
+        intensity={accent(25)}
         penumbra={1}
         color="#9d4edd"
       />
       <rectAreaLight
         color={tod.color}
-        intensity={tod.intensity}
-        width={3}
-        height={2}
-        position={[1, 3, 4]}
+        intensity={tod.intensity * 0.5}
+        width={2}
+        height={1.5}
+        position={[1, 2, 3]}
         rotation={[-Math.PI / 4, Math.PI / 4, 0]}
       />
       <pointLight
-        position={[0, 1, 0]}
-        intensity={accent(10)}
+        position={[0, 0.5, 0]}
+        intensity={accent(8)}
         color="#7209b7"
       />
       <pointLight
-        position={[1, 2, -2]}
-        intensity={accent(10)}
+        position={[1, 1, -1.5]}
+        intensity={accent(8)}
         color="#0d00a4"
       />
     </>
--- src/components/HeroModels/MyComputer.tsx
+++ src/components/HeroModels/MyComputer.tsx
@@ -17,11 +17,16 @@ export function MyComputer(props: JSX.IntrinsicElements["group"]) {
     "/models/my_computer.glb",
   ) as unknown as GLTFResult;
 
+  const computerMaterial = materials.palette.clone();
+  computerMaterial.roughness = 0.4;
+  computerMaterial.metalness = 0.3;
+  computerMaterial.envMapIntensity = 0.6;
+
   return (
     <group {...props}>
       <mesh
         geometry={nodes.Object_2.geometry}
-        material={materials.palette}
+        material={computerMaterial}
         rotation={[-Math.PI / 2, 0, 0]}
       />
     </group>
